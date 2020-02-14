de9626b38f2acc76eb4c6ae1d58fbd0ae7c1bb59
Florian COMPIEGNE <florian.compiegne@eemi.com>
2020-02-14 11:06:36 +0100

    fichier bdd

--- script.py
+++ script.py
@@ -31,7 +31,5 @@ df = pd.read_csv("file.csv", encoding='utf-8', sep=";", na_values=['_______'])
 print(df.columns)
 df.drop(['n_amenageur', 'source'], axis=1, inplace=True)  # drop column, inplace true for reasign the dataFrame for df
 df.replace('', np.nan, inplace=True)
-print(df)
 
-# print(df.head(5))
-# df.head(5).to_csv('out.csv', encoding='utf-8')
+df.to_csv('out.csv', encoding='utf-8')
